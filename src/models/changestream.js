b3a5153f4eb03c7147e70e2b6f19330db4809e70
Sieusaosieungo <thinhhanh1@gmail.com>
2021-04-15 09:32:50 +0700

    add message to kafka through Gateway

--- src/models/changestream.js
+++ src/models/changestream.js
@@ -1,8 +1,10 @@
+const axios = require('axios').default;
 const MongoClient = require('mongodb').MongoClient;
 const {
   MONGO_URI_MONITORED_DB,
   MONGO_DATABASE_MONITORED,
   MONGO_COLLECTIONS_MONITORED,
+  GATEWAY_URL,
 } = require('../configs');
 
 const client = new MongoClient(MONGO_URI_MONITORED_DB, {
@@ -16,8 +18,36 @@ client.connect().then(() => {
       .db(MONGO_DATABASE_MONITORED)
       .collection(collectionMonitored)
       .watch();
-    changeStream.on('change', (next) => {
-      console.log(next);
+    changeStream.on('change', async (dataChange) => {
+      console.log({ dataChange });
+      const {
+        operationType,
+        clusterTime,
+        ns,
+        fullDocument,
+        documentKey,
+      } = dataChange;
+
+      // call api logDb of Gateway to add log to Kafka
+      const body = {
+        operationType,
+        timestamp: new Date(clusterTime.getHighBits() * 1000),
+        name: ns.db,
+        coll: ns.coll,
+        documentKey: documentKey._id,
+        currData: fullDocument,
+      };
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      try {
+        await axios.post(`${GATEWAY_URL}/api/v1/log-db`, body, config);
+      } catch (error) {
+        console.log({ error });
+      }
     });
   });
 });
